be58aa095749f0314004ebddb4fd4724dc7c59e1
nainamohame <nainamohmed111@gmail.com>
2024-04-21 06:29:56 +0530

    update-project

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+
+function Search(){
+    return(
+      <div className="searchbar">
+              
+              <input type="text" placeholder="Search"/>
+              
+          </div>
+  
+    )
+  }
+
+  export default Search
